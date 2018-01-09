bfcbc7ab69bb7533c0d52f3e1f7dc8c02a301917
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-01-09 17:31:51 -0500

    Solution to question 5.

--- Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -15,7 +15,18 @@ let hasBoysAndGirlsAttending = false
 let hasGreenAndWhiteBuildings = true
 
 // Answer question 5 below
+if hasWaterfrontOnLake && hasBoysAndGirlsAttending && hasGreenAndWhiteBuildings {
+    print("Must be Lakefield!")
+} else {
+    print("Hmm, need more data to be sure.")
+}
 
+// Alternate syntax
+if hasWaterfrontOnLake == true && hasBoysAndGirlsAttending == true && hasGreenAndWhiteBuildings == true {
+    print("Must be Lakefield!")
+} else {
+    print("Hmm, need more data to be sure.")
+}
 
 /*:
  ## Now share your understanding
